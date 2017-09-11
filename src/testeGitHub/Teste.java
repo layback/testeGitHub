6c383246afcf1771a2a24c09246c328e6cf2807a
layback <diego20rodrigues@hotmail.com>
2017-09-11 15:44:05 -0300

    primeiro commit

---
+++ src/testeGitHub/Teste.java
@@ -0,0 +1,11 @@
+package testeGitHub;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		Teste1 teste1 = new Teste1();
+		teste1.setNome("teste gitttt");
+		System.out.println(teste1.getNome());
+	}
+
+}
---
+++ src/testeGitHub/Teste1.java
@@ -0,0 +1,14 @@
+package testeGitHub;
+
+public class Teste1 {
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
