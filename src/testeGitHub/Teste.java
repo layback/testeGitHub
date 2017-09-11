d50892c51609c575936f367328d3674af121d00f
diego <diego@diego-VirtualBox>
2017-09-11 17:04:21 -0300

    novo metodo

--- src/testeGitHub/Teste.java
+++ src/testeGitHub/Teste.java
@@ -5,7 +5,11 @@ public class Teste {
 	public static void main(String[] args) {
 		Teste1 teste1 = new Teste1();
 		teste1.setNome("teste gitttt");
-		System.out.println(teste1.getNome());
+		print(teste1.getNome());
+	}
+	
+	public static void print(String str) {
+		System.out.println(str);
 	}
 
 }
